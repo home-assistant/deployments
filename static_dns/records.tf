a36f95088e864aa09d069feb439d239f19b85be3
Ludeeus <ludeeus@ludeeus.dev>
2022-04-13 10:30:55 +0000

    Use 1.1.1.1

--- static_dns/records.tf
+++ static_dns/records.tf
@@ -2,7 +2,7 @@
 resource "cloudflare_record" "_checkdns" {
   zone_id = data.cloudflare_zone.dns_zone.id
   name    = "_checkdns"
-  value   = "127.0.0.1"
+  value   = "1.1.1.1"
   type    = "A"
   ttl     = 1
   proxied = false
