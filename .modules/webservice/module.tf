0a57f87d7e6bc7ccd76beb65ae0b40f38a84a322
Ludeeus <ludeeus@ludeeus.dev>
2022-04-26 16:44:11 +0000

    Pass in the container_version

--- .modules/webservice/module.tf
+++ .modules/webservice/module.tf
@@ -6,13 +6,14 @@ data "tfe_outputs" "infrastructure" {
 module "webservice" {
   source = "../service"
 
-  service_name    = var.service_name
-  container_image = var.container_image
-  launch_type     = var.launch_type
-  region          = var.region
-  ecs_cpu         = var.ecs_cpu
-  ecs_memory      = var.ecs_memory
-  webservice      = true
+  service_name      = var.service_name
+  container_image   = var.container_image
+  container_version = var.container_version
+  launch_type       = var.launch_type
+  region            = var.region
+  ecs_cpu           = var.ecs_cpu
+  ecs_memory        = var.ecs_memory
+  webservice        = true
   container_definitions = merge({
     portMappings = [
       {
