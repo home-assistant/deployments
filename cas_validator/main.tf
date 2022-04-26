7ca8a8b8a41e56f5dee4d9a969d8b7ea90fd84a6
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-04-26 18:37:55 +0200

    Add container_version to service modules (#11)

--- cas_validator/main.tf
+++ cas_validator/main.tf
@@ -27,16 +27,20 @@ provider "aws" {
 module "webservice_cas_validator" {
   source = "../.modules/webservice"
 
-  service_name    = "CAS-Validator"
-  subdomain       = "cas-validator"
-  container_image = "codenotary/immuproof"
-  port            = 8091
-  container_definitions = { environment : [
-    { name : "IMMUPROOF_HOST", value : "cas.codenotary.com" },
-    { name : "IMMUPROOF_PORT", value : "443" },
-    { name : "IMMUPROOF_API_KEY", value : var.cas_api_key },
-    { name : "IMMUPROOF_WEB_TITLE_TEXT", value : "Home Assistant service validator" },
-    { name : "IMMUPROOF_WEB_HOSTED_BY_TEXT", value : "Home Assistant Community" },
-    { name : "IMMUPROOF_WEB_HOSTED_BY_LOGO_URL", value : "https://www.home-assistant.io/images/home-assistant-logo.svg" }
-  ] }
+  service_name      = "CAS-Validator"
+  subdomain         = "cas-validator"
+  container_image   = "codenotary/immuproof"
+  container_version = "v0.0.11"
+  port              = 8091
+
+  container_definitions = {
+    environment : [
+      { name : "IMMUPROOF_HOST", value : "cas.codenotary.com" },
+      { name : "IMMUPROOF_PORT", value : "443" },
+      { name : "IMMUPROOF_API_KEY", value : var.cas_api_key },
+      { name : "IMMUPROOF_WEB_TITLE_TEXT", value : "Home Assistant service validator" },
+      { name : "IMMUPROOF_WEB_HOSTED_BY_TEXT", value : "Home Assistant Community" },
+      { name : "IMMUPROOF_WEB_HOSTED_BY_LOGO_URL", value : "https://www.home-assistant.io/images/home-assistant-logo.svg" }
+    ]
+  }
 }
