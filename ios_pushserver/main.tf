5abe46c3a04daca7f77df322695d683f9a05a1f8
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-05-19 10:42:09 +0200

    Add iOS-PushServer deployment (#13)

---
+++ ios_pushserver/main.tf
@@ -0,0 +1,43 @@
+terraform {
+  cloud {
+    organization = "home_assistant"
+
+    workspaces {
+      name = "ios_pushserver"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "webservice_ios_pushserver" {
+  source = "../.modules/webservice"
+
+  service_name      = "iOS-PushServer"
+  container_image   = "ghcr.io/home-assistant/ios-pushserver"
+  container_version = "0.1.0"
+  port              = 8080
+
+  container_definitions = {
+    environment : [
+      { name : "APNS_TOPIC", value : var.apns_topic },
+      { name : "APNS_KEY_CONTENTS", value : var.apns_key_contents },
+      { name : "APNS_KEY_IDENTIFIER", value : var.apns_key_identifier },
+      { name : "APNS_KEY_TEAM_IDENTIFIER", value : var.apns_key_team_identifier }
+    ]
+  }
+}
---
+++ ios_pushserver/variables.tf
@@ -0,0 +1,15 @@
+variable "apns_topic" {
+  type = string
+}
+
+variable "apns_key_contents" {
+  type = string
+}
+
+variable "apns_key_identifier" {
+  type = string
+}
+
+variable "apns_key_team_identifier" {
+  type = string
+}
