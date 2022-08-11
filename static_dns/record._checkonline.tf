f619c1f4e0c40205e54a3a235aaa173fd7f9799d
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-08-11 12:37:39 +0200

    Remove _checkonline (#20)

--- static_dns/record._checkonline.tf
+++
@@ -1,9 +0,0 @@
-
-resource "cloudflare_record" "_checkonline" {
-  zone_id = data.cloudflare_zone.dns_zone.id
-  name    = "_checkonline"
-  value   = "version.${var.domain_name}"
-  type    = "CNAME"
-  ttl     = 1
-  proxied = true
-}
