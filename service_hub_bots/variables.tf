573c0d8970543531004ef103c4d2486272c77ae4
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-09-13 11:31:03 +0200

    Bump service-hub-bots to 0.1.0 (#27)

--- service_hub_bots/variables.tf
+++ service_hub_bots/variables.tf
@@ -1,7 +1,19 @@
+variable "discord_token" {
+  type = string
+}
+
+variable "discord_guild_id" {
+  type = string
+}
+
 variable "github_token" {
   type = string
 }
 
 variable "github_webhook_secret" {
+  type = string
+}
+
+variable "sentry_dsn" {
   type = string
 }
