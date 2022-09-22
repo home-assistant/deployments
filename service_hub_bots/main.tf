2ff6e3e7c6a8b2c3937f3f4a5fcb052bb7333a34
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-09-22 12:58:38 +0200

    Deploy service-hub-bots with version 0.3.3

--- service_hub_bots/main.tf
+++ service_hub_bots/main.tf
@@ -29,7 +29,7 @@ module "webservice_service_hub_bots" {
 
   service_name      = "service-hub-bots"
   container_image   = "ghcr.io/home-assistant/service-hub"
-  container_version = "0.3.1"
+  container_version = "0.3.3"
   port              = 5000
   healthcheck_path  = "/__heartbeat__"
 
