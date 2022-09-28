bdab51c27756b9afcc6d5967e9d9f22324aac961
Ludeeus <ludeeus@ludeeus.dev>
2022-09-28 13:10:06 +0000

    Move iOS push to service-hub

--- ios_pushserver/variables.tf
+++ ios_pushserver/variables.tf
@@ -13,3 +13,11 @@ variable "apns_key_identifier" {
 variable "apns_key_team_identifier" {
   type = string
 }
+
+variable "sentry_dsn" {
+  type = string
+}
+
+variable "service_hub_image_tag" {
+  type = string
+}
