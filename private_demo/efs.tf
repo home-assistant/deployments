5ef6bc026e9a747979386cf99d47a72d998d6e26
Ludeeus <ludeeus@ludeeus.dev>
2022-09-29 15:12:51 +0000

    Add efs sg

--- private_demo/efs.tf
+++ private_demo/efs.tf
@@ -1,7 +1,23 @@
 
 resource "aws_efs_file_system" "efs" {
   creation_token = "efs"
+
+  tags = {
+    Name = "PrivateDemo"
+  }
 }
+
+
+resource "aws_security_group" "efs" {
+  vpc_id      = data.tfe_outputs.infrastructure.values["us-east-1"].network_id
+
+  ingress {
+    from_port = 2049
+    to_port   = 2049
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+ }
 
 resource "aws_efs_mount_target" "mount" {
   for_each = toset(
@@ -10,5 +26,6 @@ resource "aws_efs_mount_target" "mount" {
 
   file_system_id = aws_efs_file_system.efs.id
   subnet_id      = each.value
+  security_groups = [aws_security_group.efs.id]
 
 }
