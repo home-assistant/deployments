8208b6f1ee7d6ed16301c5e5a39e30a0e859a150
Ludeeus <ludeeus@ludeeus.dev>
2022-09-29 15:14:05 +0000

    lint

--- private_demo/efs.tf
+++ private_demo/efs.tf
@@ -9,23 +9,23 @@ resource "aws_efs_file_system" "efs" {
 
 
 resource "aws_security_group" "efs" {
-  vpc_id      = data.tfe_outputs.infrastructure.values["us-east-1"].network_id
+  vpc_id = data.tfe_outputs.infrastructure.values["us-east-1"].network_id
 
   ingress {
-    from_port = 2049
-    to_port   = 2049
-    protocol  = "tcp"
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
- }
+}
 
 resource "aws_efs_mount_target" "mount" {
   for_each = toset(
     nonsensitive(data.tfe_outputs.infrastructure.values["us-east-1"].private_subnets),
   )
 
-  file_system_id = aws_efs_file_system.efs.id
-  subnet_id      = each.value
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = each.value
   security_groups = [aws_security_group.efs.id]
 
 }
