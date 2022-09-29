df49df3daadf759e622f2933d10311f1719c8d43
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-09-29 17:15:05 +0200

    Add efs sg (#45)
    * Add efs sg
    
    * lint

--- private_demo/efs.tf
+++ private_demo/efs.tf
@@ -1,6 +1,22 @@
 
 resource "aws_efs_file_system" "efs" {
   creation_token = "efs"
+
+  tags = {
+    Name = "PrivateDemo"
+  }
+}
+
+
+resource "aws_security_group" "efs" {
+  vpc_id = data.tfe_outputs.infrastructure.values["us-east-1"].network_id
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
 resource "aws_efs_mount_target" "mount" {
@@ -8,7 +24,8 @@ resource "aws_efs_mount_target" "mount" {
     nonsensitive(data.tfe_outputs.infrastructure.values["us-east-1"].private_subnets),
   )
 
-  file_system_id = aws_efs_file_system.efs.id
-  subnet_id      = each.value
+  file_system_id  = aws_efs_file_system.efs.id
+  subnet_id       = each.value
+  security_groups = [aws_security_group.efs.id]
 
 }
