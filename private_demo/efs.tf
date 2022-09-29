47497b1fb5182c486bb304aed6616ca7917af8cf
Ludeeus <ludeeus@ludeeus.dev>
2022-09-29 15:32:58 +0000

    Add egress to efs sg

--- private_demo/efs.tf
+++ private_demo/efs.tf
@@ -11,6 +11,13 @@ resource "aws_efs_file_system" "efs" {
 resource "aws_security_group" "efs" {
   vpc_id = data.tfe_outputs.infrastructure.values["us-east-1"].network_id
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   ingress {
     from_port   = 2049
     to_port     = 2049
