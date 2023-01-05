c07b6e20eebef0b2ad7cac65dfaeba3069162802
Pierre <397503+bemble@users.noreply.github.com>
2023-01-05 11:34:27 +0100

    feat(community): add discourse server (#56)
    * feat(community): add discourse server
    
    * fix(community): remove instance id from tf file
    
    * feat(community): move instance into dedicated tf file
    
    * feat(community): add tags to server
    
    * remove quotes

---
+++ community/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  cloud {
+    organization = "home_assistant"
+
+    workspaces {
+      name = "community"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-2"
+}
