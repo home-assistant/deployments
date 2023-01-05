956f4c4560476727cd9a25364dd392115709aa96
Pierre <397503+bemble@users.noreply.github.com>
2023-01-05 10:02:56 +0000

    feat(community): add tags to server

--- community/ec2.tf
+++ community/ec2.tf
@@ -2,4 +2,10 @@
 resource "aws_instance" "discourse" {
   ami           = "ami-003634241a8fcdec0"
   instance_type = "m5a.4xlarge"
+  tags = {
+    "Name" = "Discourse"
+  }
+  tags_all = {
+    "Name" = "Discourse"
+  }
 }
