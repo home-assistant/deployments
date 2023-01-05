e98fd264cf16a5c6f9a076681c74b4614f04c124
Pierre <397503+bemble@users.noreply.github.com>
2023-01-05 10:12:58 +0000

    remove quotes

--- community/ec2.tf
+++ community/ec2.tf
@@ -3,9 +3,9 @@ resource "aws_instance" "discourse" {
   ami           = "ami-003634241a8fcdec0"
   instance_type = "m5a.4xlarge"
   tags = {
-    "Name" = "Discourse"
+    Name = "Discourse"
   }
   tags_all = {
-    "Name" = "Discourse"
+    Name = "Discourse"
   }
 }
