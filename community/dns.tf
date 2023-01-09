53280795b31426b83c22b00d2eb4aaad9d83235c
Pierre <397503+bemble@users.noreply.github.com>
2023-01-09 08:37:17 +0000

    feat(community): delete community-direct entry

--- community/dns.tf
+++ community/dns.tf
@@ -9,11 +9,3 @@ resource "cloudflare_record" "community" {
   type    = "A"
   proxied = true
 }
-
-resource "cloudflare_record" "community_direct" {
-  zone_id = data.cloudflare_zone.dns_zone.id
-  name    = "community-direct"
-  value   = aws_eip.discourse.public_ip
-  type    = "A"
-  proxied = false
-}
