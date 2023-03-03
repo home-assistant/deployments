a56fab0c9128a0613d9b5e5a6ed3d4934bc621df
Joakim Sørensen <ludeeus@ludeeus.dev>
2023-03-03 15:00:48 +0100

    Bump private demo to 2023.3.1 (#61)

--- private_demo/main.tf
+++ private_demo/main.tf
@@ -34,7 +34,7 @@ module "webservice_private_demo" {
 
   service_name      = "Private-Demo"
   container_image   = "ghcr.io/home-assistant/private-demo"
-  container_version = "2023.2.2.1"
+  container_version = "2023.3.1"
   port              = 8123
 
   container_volumes = [
