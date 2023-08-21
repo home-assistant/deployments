6b480808039cd638c8a736e3db0c0a8949d8555c
ludeeus <joasoe@gmail.com>
2023-08-21 11:34:57 +0000

    Add assist test bucket

--- assist/bucket.tf
+++ assist/bucket.tf
@@ -3,3 +3,9 @@ resource "cloudflare_r2_bucket" "assist_wakeword_training_data" {
   name       = "assist-wakeword-training-data"
   location   = "ENAM"
 }
+
+resource "cloudflare_r2_bucket" "assist_wakeword_training_data_test" {
+  account_id = var.CLOUDFLARE_ACCOUNT_ID
+  name       = "assist-wakeword-training-data-test"
+  location   = "ENAM"
+}
