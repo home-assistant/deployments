bba30d2ee5881c34a9fb304a6e28b024826435ea
ludeeus <joasoe@gmail.com>
2023-10-05 13:17:55 +0000

    Add os-build-cache bucket

--- os-builds/bucket.tf
+++ os-builds/bucket.tf
@@ -3,3 +3,9 @@ resource "cloudflare_r2_bucket" "os_build_artifacts" {
   name       = "os-build-artifacts"
   location   = "ENAM"
 }
+
+resource "cloudflare_r2_bucket" "os_build_cache" {
+  account_id = var.CLOUDFLARE_ACCOUNT_ID
+  name       = "os-build-cache"
+  location   = "ENAM"
+}
