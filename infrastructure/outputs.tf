3ff4dbca68867c619c6ae125afdee65f8a425d14
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2024-09-23 18:07:53 +0300

    Add support for additional deployment regions

--- infrastructure/outputs.tf
+++ infrastructure/outputs.tf
@@ -12,3 +12,23 @@ output "us-east-1" {
     "network_id" : module.us_east_1.network_id,
   }
 }
+
+output "eu-central-1" {
+  description = "Outputs for the eu-central-1 region"
+  value = {
+    "public_subnets" : module.eu_central_1.public_subnets,
+    "private_subnets" : module.eu_central_1.private_subnets,
+    "ecs_cluster" : module.eu_central_1.ecs_cluster,
+    "network_id" : module.eu_central_1.network_id,
+  }
+}
+
+output "ap-southeast-1" {
+  description = "Outputs for the ap-southeast-1 region"
+  value = {
+    "public_subnets" : module.ap_southeast_1.public_subnets,
+    "private_subnets" : module.ap_southeast_1.private_subnets,
+    "ecs_cluster" : module.ap_southeast_1.ecs_cluster,
+    "network_id" : module.ap_southeast_1.network_id,
+  }
+}
