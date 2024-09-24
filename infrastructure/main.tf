dd0554c097c1d0d436e532d0b9a85baeac8d4341
Krisjanis Lejejs <krisjanis.lejejs@nabucasa.com>
2024-09-24 15:57:30 +0300

    Add support for additional deployment regions (#88)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,3 +18,17 @@ module "us_east_1" {
   ecs_policy   = aws_iam_instance_profile.ecs_instance_profile.arn
   network_cidr = var.network_cidr["us-east-1"]
 }
+
+module "eu_central_1" {
+  source       = "./region"
+  region       = "eu-central-1"
+  ecs_policy   = aws_iam_instance_profile.ecs_instance_profile.arn
+  network_cidr = var.network_cidr["eu-central-1"]
+}
+
+module "ap_southeast_1" {
+  source       = "./region"
+  region       = "ap-southeast-1"
+  ecs_policy   = aws_iam_instance_profile.ecs_instance_profile.arn
+  network_cidr = var.network_cidr["ap-southeast-1"]
+}
