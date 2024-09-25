d19aa7ddbb987ebd671e6c6556bc93d0247c9884
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2024-09-25 16:11:59 +0300

    PR review fixes

--- .modules/webservice/dns.tf
+++ .modules/webservice/dns.tf
@@ -5,7 +5,7 @@ data "cloudflare_zone" "dns_zone" {
 resource "cloudflare_record" "instance_dns" {
   zone_id = data.cloudflare_zone.dns_zone.id
   name    = coalesce(var.subdomain, lower(var.service_name))
-  content = lower(aws_alb.main.dns_name)
+  value   = lower(aws_alb.main.dns_name)
   type    = "CNAME"
   ttl     = 1
   proxied = var.cloudflare_proxy
