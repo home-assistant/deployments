7682d8bf50fb1dba84e99bea8ae169fb92769543
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2024-09-27 11:12:55 +0300

    Update Terraform versions and dependencies

--- .modules/webservice/versions.tf
+++ .modules/webservice/versions.tf
@@ -1,8 +1,10 @@
 terraform {
+  required_version = "= 1.9.6"
+
   required_providers {
     cloudflare = {
       source  = "cloudflare/cloudflare"
-      version = "~> 3.0"
+      version = "~> 4.0"
     }
   }
 }
--- static_dns/record._checkdns.tf
+++ static_dns/record._checkdns.tf
@@ -2,7 +2,7 @@
 resource "cloudflare_record" "_checkdns" {
   zone_id = data.cloudflare_zone.dns_zone.id
   name    = "_checkdns"
-  value   = "1.1.1.1"
+  content = "1.1.1.1"
   type    = "A"
   ttl     = 1
   proxied = false
