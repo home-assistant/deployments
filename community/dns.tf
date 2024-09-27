9035b5f3d35bd59b21a3901f96181c7de2d6d041
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2024-09-27 11:32:35 +0300

    Update community versions

--- community/dns.tf
+++ community/dns.tf
@@ -5,7 +5,7 @@ data "cloudflare_zone" "dns_zone" {
 resource "cloudflare_record" "community" {
   zone_id = data.cloudflare_zone.dns_zone.id
   name    = "community"
-  value   = aws_eip.discourse.public_ip
+  content = aws_eip.discourse.public_ip
   type    = "A"
   proxied = true
 }
