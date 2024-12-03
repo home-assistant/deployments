b420e2fe8f85d3d85edcdd515d5720355ce3c27d
Pierre <397503+bemble@users.noreply.github.com>
2024-12-03 15:53:27 +0100

    fix resource name

--- ohf-public-assets/bucket.tf
+++ ohf-public-assets/bucket.tf
@@ -1,4 +1,4 @@
-resource "cloudflare_r2_bucket" "os_build_artifacts" {
+resource "cloudflare_r2_bucket" "ohf_public_assets" {
   account_id = var.CLOUDFLARE_ACCOUNT_ID
   name       = "ohf-public-assets"
   location   = "ENAM"
