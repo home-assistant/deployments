ba8ea9eb2e7500ed4204d0d195f70bcf265cb185
Krisjanis Lejejs <krisjanis.lejejs@gmail.com>
2025-01-07 11:43:50 +0200

    Fix pool description

--- .modules/cloudflare/load_balancer_pool/dns.tf
+++ .modules/cloudflare/load_balancer_pool/dns.tf
@@ -18,7 +18,7 @@ locals {
 resource "cloudflare_load_balancer_pool" "pool" {
   account_id  = var.cloudflare_account_id
   name        = "${var.pool_name}-${var.region}"
-  description = "Load balancer pool ${var.pool_name} for ${var.region}"
+  description = "Pool for ${var.pool_name} load balancer in ${var.region}"
   latitude    = local.geolocation[var.region].latitude
   longitude   = local.geolocation[var.region].longitude
   monitor     = var.load_balancer_monitor_id
