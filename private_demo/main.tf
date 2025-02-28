59d9c46225cee6943dcfc76c3ebba24788a96842
Krisjanis Lejejs <krisjanis.lejejs@nabucasa.com>
2025-02-28 15:42:57 +0200

    Update private_demo versions (#102)
    * Update private_demo versions
    
    * Upgrade Terraform version

--- private_demo/main.tf
+++ private_demo/main.tf
@@ -7,10 +7,12 @@ terraform {
     }
   }
 
+  required_version = "~> 1.11.0"
+
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = "~> 5.0"
     }
 
     cloudflare = {
