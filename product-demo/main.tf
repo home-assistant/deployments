5e374029606a90333dd15bf20ad54a643acf2eba
Marcos Pérez Ferro <djwedo@gmail.com>
2026-02-18 18:25:43 +0100

    Add product demo configuration with Terraform files for AWS and Cloudflare - based on marketplace demo

---
+++ product-demo/main.tf
@@ -0,0 +1,58 @@
+terraform {
+  cloud {
+    organization = "home_assistant"
+
+    workspaces {
+      name = "product_demo"
+    }
+  }
+
+  required_version = "~> 1.14.3"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "eu-north-1"
+
+  default_tags {
+    tags = {
+      application = "product-demo"
+    }
+  }
+}
+
+data "tfe_outputs" "infrastructure" {
+  organization = "home_assistant"
+  workspace    = "infrastructure"
+}
+
+module "webservice_product_demo" {
+  source = "../.modules/webservice"
+
+  service_name      = "Product-Demo"
+  container_image   = "ghcr.io/home-assistant/marketplace-demo"
+  container_version = var.image_tag
+  port              = 8123
+  cloudflare_proxy  = false
+  ecs_memory        = 2048
+
+  container_definitions = {
+    environment = [
+      {
+        name  = "DEFAULT_USERS"
+        value = var.default_users
+      }
+    ]
+  }
+}
