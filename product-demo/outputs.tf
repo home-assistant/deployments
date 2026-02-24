de9de00dc789c6a4b304f3eb643ac876dbd5a037
Marcos Pérez Ferro <djwedo@gmail.com>
2026-02-24 16:43:05 +0100

    feat: add output for configuration.yaml in product demo

--- product-demo/outputs.tf
+++ product-demo/outputs.tf
@@ -7,3 +7,8 @@ output "fqdn" {
   description = "FQDN of the service"
   value       = module.webservice_product_demo.fqdn
 }
+
+output "configuration_yaml" {
+  description = "Content of configuration.yaml in use by the deployment"
+  value       = var.configuration_yaml
+}
